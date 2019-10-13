0c09351ce2c12203f9dc4ad5496903b8915ca288
nslallu <55460036+nslallu@users.noreply.github.com>
2019-10-12 21:21:01 -0400

    sign in page changes

--- signin/js/signin.js
+++ signin/js/signin.js
@@ -12,29 +12,48 @@ $(function () {
 	sDateofBirth.datepicker(options);	
 	
 	$("#formSignUp").on("submit", function (e) {
-		console.log('clicked on sign up!');
+		e.preventDefault(); 
 		var data = {
 			FirstName: $("#inputFirstName").val(),
 			LastName: $("#inputLastName").val(),
 			Email: $("#inputEmailAddr").val(),
 			Password: $("#inputPswd").val().toString(),
-			DateOfBirth: $("#inputDateOfbirth").data().datepicker.getDate(),
-			Gender: $("#inputGenderMale")[0].checked ? "m" : "f"
+			DateOfBirth: $("#inputDateOfbirth").data().datepicker.getFormattedDate(),
+			Gender: $("#inputGenderMale")[0].checked ? "m" : "f",
+			Nationality: "IN"
 		};
-		console.log(data);
 		$.ajax({
             type: "POST",
             url: "http://40.121.195.146:8000/api-gateway.php/penn-connect/user",
-            data: data,
-	    dataType: "json",
+            data: JSON.stringify(data),
             success: function () {
-                   console.log("user signed up!");
+				$('#formSignUp').get(0).reset();
+                window.location = '../newsfeed/index.html';
             },
-	    error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-	    }
+			error: function(xhr, resp, text) {
+				
+			}
+        });     
+	});
 
-
+	$("#formSignIn").on("submit", function (e) {
+		e.preventDefault(); 
+		var data = {
+			Email: $("#inputEmailAddr").val(),
+			Password: $("#inputPswd").val().toString()
+		};
+		$.ajax({
+            type: "POST",
+            url: "http://40.121.195.146:8000/api-gateway.php/penn-connect/login",
+            data: JSON.stringify(data),
+            success: function () {
+				$("#alert").hide();
+				$('#formSignIn').get(0).reset();
+                window.location = '../newsfeed/index.html';
+            },
+			error: function(xhr, resp, text) {
+				$("#alert").show();
+			}
         });     
 	});
 });
